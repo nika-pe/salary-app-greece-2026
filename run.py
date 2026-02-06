8ba8a479c5dc51e40bd1f14b41d16c426fd83145
domenika <niki7555@gmail.com>
2026-02-07 00:36:21 +0200

    Fix port binding logic in run.py and Procfile

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from app import create_app
 app = create_app()
-import os
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
